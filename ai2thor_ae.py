94625f66bb47f341906e36843df71fa9a21bbb06
Arturs Elksnis <archie1983@hotmail.co.uk>
2025-07-09 23:33:08 +0100

    First iteration of the AI2Thor environment, but not debugged yet.

--- ai2thor_ae.py
+++ ai2thor_ae.py
@@ -8,189 +8,334 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-#from ai2_thor_model_training.training_data_extraction import (RobotNavigationControl, action_mapping,
-#                            action_to_index, index_to_action, inverted_action_mapping)
-from ai2_thor_model_training.training_data_extraction import (index_to_action)
+## AE: From my AI2-Thor simulation packages and for their purposes
+from ai2_thor_model_training.training_data_extraction import (RobotNavigationControl, action_mapping,
+                                                              action_to_index, index_to_action, inverted_action_mapping,
+                                                              AI2THORUtils, get_path_length, get_centre_of_the_room,
+                                                              room_this_point_belongs_to, get_rooms_ground_truth)
+
+from thortils import launch_controller
+from thortils.utils.math import sep_spatial_sample
+import thortils as tt
+import random, cv2
+from thortils.agent import thor_agent_pose, thor_pose_as_tuple
+from thortils.controller import _resolve
+from thortils.navigation import get_shortest_path_to_object
+
+#from thortils.agent import thor_reachable_positions, thor_agent_position, thor_agent_pose
+#from thortils.utils import roundany
+
+
+
+from PIL import Image
+
 
 class AI2ThorEnv():
+    TOKENIZER = re.compile(r'([A-Za-z_]+|[^A-Za-z_ ]+)')
 
-  TOKENIZER = re.compile(r'([A-Za-z_]+|[^A-Za-z_ ]+)')
+    def __init__(
+            self, level, repeat=4, size=(64, 64), mode='train',
+            actions='popart', episodic=True, text=None, seed=None):
 
-  def __init__(
-      self, level, repeat=4, size=(64, 64), mode='train',
-      actions='popart', episodic=True, text=None, seed=None):
-    if level == 'goals':  # Shortcut for convenience
-      level = 'dmlab_explore_goal_locations_small'
-    self._size = size
-    self._repeat = repeat
-    self._actions = {
-        'impala': IMPALA_ACTION_SET,
-        'popart': POPART_ACTION_SET,
-    }[actions]
-    if text is None:
-      text = bool(level.startswith('language'))
-    self._episodic = episodic
-    self._text = text
-    self._random = np.random.RandomState(seed)
-    config = dict(height=size[0], width=size[1], logLevel='WARN')
-    if mode == 'train':
-      if level.endswith('_test'):
-        level = level.replace('_test', '_train')
-    elif mode == 'eval':
-      config.update(allowHoldOutLevels='true', mixerSeed=0x600D5EED)
-    else:
-      raise NotImplementedError(mode)
-    config = {k: str(v) for k, v in config.items()}
-    obs = ['RGB_INTERLEAVED', 'INSTR'] if text else ['RGB_INTERLEAVED']
-    self._env = deepmind_lab.Lab(
-        level='contributed/dmlab30/' + level,
-        observations=obs, config=config)
-    self._current_image = None
-    if self._text:
-      self._current_instr = None
-      self._instr_length = 32
-      self._embed_size = 32
-      self._vocab_buckets = 64 * 1024
-      self._embeddings = np.random.default_rng(seed=0).normal(
-          0.0, 1.0, (self._vocab_buckets, self._embed_size)).astype(np.float32)
-    self._done = True
+        # AE: AI2-Thor simulation stuff
+        self.rnc = RobotNavigationControl()
+        self.controller = None
+        self.atu = AI2THORUtils()
+        self.rooms_in_habitat = None
+        self.current_path_length = 1000
 
-  @property
-  def obs_space(self):
-    spaces = {
-        'image': elements.Space(np.uint8, self._size + (3,)),
-        'reward': elements.Space(np.float32),
-        'is_first': elements.Space(bool),
-        'is_last': elements.Space(bool),
-        'is_terminal': elements.Space(bool),
-    }
-    if self._text:
-      spaces['instr'] = elements.Space(
-          np.float32, self._instr_length * self._embed_size)
-    return spaces
+        # when we select a random position and plan path to the room centre, we will assign a value to this parameter
+        # with the A* path length from that random position to the desired point. This will help calculate reward from all
+        # further points.
+        self.initial_path_length = 0
 
-  @property
-  def act_space(self):
-    return {
-        'action': elements.Space(np.int32, (), 0, len(self._actions)),
-        'reset': elements.Space(bool),
-    }
+        self.habitat_id = level
 
-  # Advances the simulation using the selected action
-  def step(self, action):
-    if action['reset'] or self._done:
-      self._env.reset(seed=self._random.randint(0, 2 ** 31 - 1))
-      self._done = False
-      return self._obs(0.0, is_first=True)
-    raw_action = np.array(self._actions[action['action']], np.intc)
-    #print(raw_action)
-    reward = self._env.step(raw_action, num_steps=self._repeat)
-    self._done = not self._env.is_running()
-    return self._obs(reward, is_last=self._done)
+        # Dreamer stuff
+        self._size = size
+        self._repeat = repeat
 
-  # Returns the observations from the last performed step
-  def _obs(self, reward, is_first=False, is_last=False):
-    if not self._done:
-      self._current_image = self._env.observations()['RGB_INTERLEAVED']
-      if self._text:
-        self._current_instr = self._embed(self._env.observations()['INSTR'])
-    obs = dict(
-        image=self._current_image,
-        reward=np.float32(reward),
-        is_first=is_first,
-        is_last=is_last,
-        is_terminal=is_last if self._episodic else False,
-    )
+        # Dreamer + AI2Thor
+        self._actions = action_mapping
+        self._actions.pop("STOP")  # remove STOP action because that will be treated differently
 
-    if self._text:
-      obs['instr'] = self._current_instr
-    return obs
+        self._episodic = episodic
+        self._random = np.random.RandomState(seed)
+        config = dict(height=size[0], width=size[1], logLevel='WARN')
+        if mode == 'train':
+            # if level.endswith('_test'):
+            #  level = level.replace('_test', '_train')
+            pass
+        elif mode == 'eval':
+            # config.update(allowHoldOutLevels='true', mixerSeed=0x600D5EED)
+            pass
+        else:
+            raise NotImplementedError(mode)
+        config = {k: str(v) for k, v in config.items()}
 
-  def _embed(self, text):
-    tokens = self.TOKENIZER.findall(text.lower())
-    indices = [self._hash(token) for token in tokens]
-    # print('EMBED', text, '->', tokens, '->', indices)
-    indices = indices + [0] * (self._instr_length - len(indices))
-    embeddings = [self._embeddings[i] for i in indices]
-    return np.concatenate(embeddings)
+        # self._env = deepmind_lab.Lab(
+        #    level='contributed/dmlab30/' + level,
+        #    observations=obs, config=config)
+        self._current_image = None
+        self._done = True
 
-  @functools.cache
-  def _hash(self, token):
-    return zlib.crc32(token.encode('utf-8')) % self._vocab_buckets
+    @property
+    def obs_space(self):
+        spaces = {
+            'image': elements.Space(np.uint8, self._size + (3,)),
+            'reward': elements.Space(np.float32),
+            'is_first': elements.Space(bool),
+            'is_last': elements.Space(bool),
+            'is_terminal': elements.Space(bool),
+        }
+        if self._text:
+            spaces['instr'] = elements.Space(
+                np.float32, self._instr_length * self._embed_size)
+        return spaces
 
-  def close(self):
-    self._env.close()
+    @property
+    def act_space(self):
+        return {
+            'action': elements.Space(np.int32, (), 0, len(self._actions)),
+            'reset': elements.Space(bool),
+        }
 
 
-# Small action set used by IMPALA.
-IMPALA_ACTION_SET = (
-    (  0, 0,  0,  1, 0, 0, 0),  # Forward
-    (  0, 0,  0, -1, 0, 0, 0),  # Backward
-    (  0, 0, -1,  0, 0, 0, 0),  # Strafe Left
-    (  0, 0,  1,  0, 0, 0, 0),  # Strafe Right
-    (-20, 0,  0,  0, 0, 0, 0),  # Look Left
-    ( 20, 0,  0,  0, 0, 0, 0),  # Look Right
-    (-20, 0,  0,  1, 0, 0, 0),  # Look Left + Forward
-    ( 20, 0,  0,  1, 0, 0, 0),  # Look Right + Forward
-    (  0, 0,  0,  0, 1, 0, 0),  # Fire
-)
 
-# Large action set used by PopArt and R2D2.
-POPART_ACTION_SET = [
-    (  0,   0,  0,  1, 0, 0, 0),  # FW
-    (  0,   0,  0, -1, 0, 0, 0),  # BW
-    (  0,   0, -1,  0, 0, 0, 0),  # Strafe Left
-    (  0,   0,  1,  0, 0, 0, 0),  # Strafe Right
-    (-10,   0,  0,  0, 0, 0, 0),  # Small LL
-    ( 10,   0,  0,  0, 0, 0, 0),  # Small LR
-    (-60,   0,  0,  0, 0, 0, 0),  # Large LL
-    ( 60,   0,  0,  0, 0, 0, 0),  # Large LR
-    (  0,  10,  0,  0, 0, 0, 0),  # Look Down
-    (  0, -10,  0,  0, 0, 0, 0),  # Look Up
-    (-10,   0,  0,  1, 0, 0, 0),  # FW + Small LL
-    ( 10,   0,  0,  1, 0, 0, 0),  # FW + Small LR
-    (-60,   0,  0,  1, 0, 0, 0),  # FW + Large LL
-    ( 60,   0,  0,  1, 0, 0, 0),  # FW + Large LR
-    (  0,   0,  0,  0, 1, 0, 0),  # Fire
-]
+
+    def process_required_habitats(self):
+        self.process_habitat(10)
+        self.controller.stop()
+
+    ##
+    # Load the given habitat- load it, and put agent in a random place
+    ##
+    def load_habitat(self, habitat_id):
+        # load required habitat
+        habitat = self.atu.load_proctor_habitat(habitat_id)
+
+        # Launch a controller for the loaded habitat. If we already have a controller,
+        # then reset it instead of loading a new one.
+        if (self.controller == None):
+            self.controller = launch_controller({"scene": habitat, "VISIBILITY_DISTANCE": 3.0, "headless": False})
+            self.rnc.set_controller(
+                self.controller)  # This allows our control scripts to interact with AI2-THOR environment
+        else:
+            self.controller.reset(habitat)
+            self.reset_state()
+            self.rnc.reset_state()
+            # self.rnc.set_controller(self.controller)
+
+        # In this habitat we have these rooms
+        self.rooms_in_habitat = get_rooms_ground_truth(habitat)
+
+        # Now place the robot in a random position and figure out the target from there.
+        self.choose_random_placement_in_habitat()
+
+    ##
+    # Here we will select a number of random placements and then choose one to navigate from it
+    # to some goal.
+    ##
+    def choose_random_placement_in_habitat(self):
+        ## All we need is a set of random positions and we get them like this:
+        # params for the random teleportation part
+        seed = 1983
+        num_stops = 20
+        num_rotates = 4
+        sep = 1.0
+        v_angles = [30]
+        h_angles = [0, 45, 90, 135, 180, 225, 270, 315]
+
+        """
+        num_stops: Number of places the agent will be placed
+        num_rotates: Number of random rotations at each place
+        sep: the minimum separation the sampled agent locations should have
+
+        kwargs: See thortils.vision.projection.open3d_pcd_from_rgbd;
+        """
+        rnd = random.Random(seed)
+
+        initial_agent_pose = tt.thor_agent_pose(self.controller)
+        initial_horizon = tt.thor_camera_horizon(self.controller.last_event)
+
+        reachable_positions = tt.thor_reachable_positions(self.controller)
+        placements = sep_spatial_sample(reachable_positions, sep, num_stops,
+                                        rnd=rnd)
+
+        #print(placements)
+
+        # Choose one placement in the set of placements and then plan path from that placement to
+        # the middle of the room. If planning path is not possible, then choose another one.
+        path_planned = False
+        while not path_planned:
+            els = elements.Space(np.int32, (), 0, len(placements))
+            p = placements[int(els.sample())]
+
+            # append a rotation to the place.
+            yaw = rnd.sample(h_angles, 1)[0]
+            place_with_rtn = p + (yaw,)
+            print("Placement: ", place_with_rtn)
+            ## Teleport, then start new exploration. Achieve goal. Then repeat.
+            self.rnc.teleport_to(place_with_rtn)
+
+            # We've just been put in a random place in a habitat. We want to move now to where we want to go,
+            # e.g., middle of the room, a door, etc.. For that we need to plan a path to there.
+            point_for_room_search = (p[0], "", p[1])
+            room_of_placement = room_this_point_belongs_to(self.rooms_in_habitat, point_for_room_search)
+            room_centre = room_of_placement[2]
+
+            # Now plan path to the centre of the room
+            try:
+                path_and_plan = self.get_path_to_target_point(room_centre)
+            except ValueError as e:
+                # If the path could not be planned, then drop it and carry on with the next one
+                print(f"ERROR: {e}")
+                continue
+
+            # print("PATH & PLAN: ", path_and_plan)
+            path = path_and_plan[0]
+            plan = path_and_plan[1]
+            #self.prev_pose = thor_agent_pose(self.controller)  # This is where we are before the plan started
+            # place_with_rtn
+            # thor_pose_as_tuple(self.prev_pose)
+            self.initial_path_length = get_path_length(path, place_with_rtn)
+
+            path_planned = True
+
+    ##
+    # Get path to an arbitrary point on the floor (e.g. center of the room)
+    ##
+    def get_path_to_target_point(self, target_point):
+        (start_position, start_rotation) = self.rnc.get_agent_pos_and_rotation()
+
+        event = _resolve(self.controller)
+        #self.last_start_position, _ = thor_agent_pose(event)
+
+        target_position_dict = {'x': target_point.x, 'y': 0.9009993672370911, 'z': target_point.y}
+        target_position_tuple = (target_position_dict['x'], target_position_dict['y'], target_position_dict['z'])
+        #print("# AE: target_position: ", target_position)
+        #print("# AE: start_pose: ", (start_position, start_rotation))
+
+        #print("AE: v_angles: " + str(v_angles) + " # h_angles: " + str(h_angles)
+        #        + " # movement_params: " + str(movement_params) + " # goal_distance: " + str(goal_distance)
+        #        + " # diagonal_ok: " + str(diagonal_ok) + " # positions_only: " + str(positions_only)
+        #        + " # return_plan: " + str(return_plan) + " # as_tuples: " + str(as_tuples))
+
+        #keywords = {'v_angles': [30], 'return_plan': True}
+        keywords = {'v_angles': [0], 'return_plan': True, 'diagonal_ok': True}
+        return get_shortest_path_to_object(self.controller, "TargetPoint", start_position, start_rotation, target_position=target_position_tuple, **keywords)
+
+    # This function will calculate path length to the desired point from the current position.
+    def get_current_path_length(self):
+        cur_pose = thor_agent_pose(self.controller)
+        cur_path = self.get_path_to_target_point()
+        cur_path = get_path_length(cur_path, cur_pose)
+
+        self.current_path_length = len(cur_path)
+
+        return self.current_path_length
+
+    # Calculates the reward for the current position of the agent. It is based on the length of the initial
+    # path and the current path to the target.
+    def current_reward(self):
+        return self.initial_path_length - self.get_current_path_length()
+
+    # Compares the current reward with the maximum reward. If they're the same, then we have arrived.
+    def have_we_arrived(self):
+        return (self.current_path_length==0)
+
+    # Advances the simulation using the selected action
+    def step(self, action):
+        # AE: If this is a terminal state or we need to end, then reset environment
+        if action['reset'] or self._done:
+            #self._env.reset(seed=self._random.randint(0, 2 ** 31 - 1))
+            self.load_habitat(self.habitat_id)
+            self._done = False
+            return self._obs(0.0, is_first=True)
+        #raw_action = np.array(self._actions[action['action']], np.intc)
+        raw_action = index_to_action(int(action['action']))
+        # print(raw_action)
+        #reward = self._env.step(raw_action, num_steps=self._repeat)
+        reward = self.current_reward()
+        self.rnc.execute_action(raw_action)
+        self._done = self.have_we_arrived()
+        return self._obs(reward, is_last=self._done)
+
+    # Returns the observations from the last performed step
+    def _obs(self, reward, is_first=False, is_last=False):
+        if not self._done:
+            self._current_image = self._env.observations()['RGB_INTERLEAVED']
+            if self._text:
+                self._current_instr = self._embed(self._env.observations()['INSTR'])
+        obs = dict(
+            image=self._current_image,
+            reward=np.float32(reward),
+            is_first=is_first,
+            is_last=is_last,
+            is_terminal=is_last if self._episodic else False,
+        )
+
+        if self._text:
+            obs['instr'] = self._current_instr
+        return obs
+
+    def _embed(self, text):
+        tokens = self.TOKENIZER.findall(text.lower())
+        indices = [self._hash(token) for token in tokens]
+        # print('EMBED', text, '->', tokens, '->', indices)
+        indices = indices + [0] * (self._instr_length - len(indices))
+        embeddings = [self._embeddings[i] for i in indices]
+        return np.concatenate(embeddings)
+
+    @functools.cache
+    def _hash(self, token):
+        return zlib.crc32(token.encode('utf-8')) % self._vocab_buckets
+
+    def close(self):
+        self._env.close()
 
 if __name__ == "__main__":
     # AE: A word about actions:
     ## AE: There is no documentation or useful comments in this code or elements package, so I have to guess,
     # but it looks like this is a uniform space generator within the specified limits., so for example
-    # if we have els = elements.Space(np.int32, (), 0, 2), then that should generate an array of [0, 1, 2].
+    # if we have els = elements.Space(np.int32, (), 0, 3), then that should generate an array of [0, 1, 2].
     # We can then call els.sample() and it will return a random element from that uniform space (array).
-    els = elements.Space(np.int32, (), 0, 7)
-    #print("els.sample(): ", els.sample())
+    # els = elements.Space(np.int32, (), 0, 7)
+    # print("els.sample(): ", els.sample())
 
     # AE: This creates an action space- a dictionary of two normal distributions- one labelled "action" and
     # the other: "reset".
-    act_space = {
-        'action': els,
-        'reset': elements.Space(bool),
-    }
+    # act_space = {
+    #    'action': els,
+    #    'reset': elements.Space(bool),
+    # }
     # Now we go through both normal distributions and choose a random member in either of them and put that under
     # the dictionary keyword. I.e., we are choosing a random action and a random reset flag, although since the
     # reset flag is chosen from only 1 member (which is True in this case), then it is not really random, but always
     # True.
-    act = {k: v.sample() for k, v in act_space.items()}
+    # act = {k: v.sample() for k, v in act_space.items()}
 
-    #print("act", act)
-    #print("act['action'], act['reset']", act['action'], act['reset'])
+    # print("act", act)
+    # print("act['action'], act['reset']", act['action'], act['reset'])
 
-    #bels = elements.Space(bool)
-    #print("bels: ", bels, " bels.sample(): ", bels.sample())
+    # bels = elements.Space(bool)
+    # print("bels: ", bels, " bels.sample(): ", bels.sample())
 
     els = elements.Space(np.int32, (), 0, 3)
     act_space = {
         'action': els,
         'reset': elements.Space(bool),
     }
+    dml = AI2ThorEnv(10)
+
     for i in range(10):
         act = {k: v.sample() for k, v in act_space.items()}
         print(act)
         a = index_to_action(int(act['action']))
         print(a)
+        act['reset'] = False
+        observation = dml.step(act)
+
 
     # dml = AI2ThorEnv("rooms_watermaze")
     # print(dml.act_space)
